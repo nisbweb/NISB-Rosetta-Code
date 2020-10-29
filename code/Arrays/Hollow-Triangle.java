8821b36e35e942904b994a619d1c0bd50d679d10
Vaishnavi Rathi <vaishnavisureshrathi@gmail.com>
2020-10-29 16:26:51 +0530

    pattern printing

---
+++ code/Arrays/Hollow-Triangle.java
@@ -0,0 +1,27 @@
+package patterns;
+import java.util.Scanner;
+public class HollowTrianglr {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.print("* ");
+		System.out.println();
+		for(int i=2;i<=n-1;i++) {
+			System.out.print("* ");
+			for(int j=1;j<=i-2;j++) {
+				System.out.print("  ");
+			}
+			System.out.print("* ");
+			System.out.println();
+		}
+		if(n>1) {
+		for(int i=1;i<=n;i++) {
+			System.out.print("* ");
+			}
+		
+		}
+
+	}
+
+}
