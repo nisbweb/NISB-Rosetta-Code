54b2693d9b02709b3678ce1485e62ce59656ce57
shreyasda <shreyasshrivatsa@gmail.com>
2022-10-18 23:36:40 +0530

    Mergesort in Python

---
+++ code/Sorting algorithms/mergesort.py
@@ -0,0 +1,33 @@
+def mergesort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergesort(L)
+        mergesort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i+=1
+            else:
+                arr[k] = R[j]
+                j+=1
+            k+=1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i+=1
+            k+=1
+        while j < len(R):
+            arr[k] = R[j]
+            j+=1
+            k+=1
+
+
+numbers = [80, 9, 16, 56, 32, 100, 27, 45]
+print(numbers)
+mergesort(numbers)
+print(numbers)
